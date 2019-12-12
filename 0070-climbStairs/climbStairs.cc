bc811e90fa48f3735e6f31b720e1df2393be1300
wangnengjie <751614701@qq.com>
2019-12-12 15:25:01 +0800

    70

---
+++ 0070-climbStairs/climbStairs.cc
@@ -0,0 +1,17 @@
+#include <vector>
+using std::vector;
+
+class Solution {
+  public:
+    int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        vector<int> arr(n);
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n - 1];
+    }
+};
