31a9713d401c5e89e0d9ba21b04118f46cbaeefa
wangnengjie <751614701@qq.com>
2020-07-28 10:06:23 +0800

    104

---
+++ 0104/maxDepth.cc
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <cstddef>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+  public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL) {
+            return 0;
+        }
+        return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
