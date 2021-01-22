f00eb6daa6ec27e027a8caf6ecd289c7ad1c4761
wangnengjie <751614701@qq.com>
2021-01-22 14:12:17 +0800

    jz27

---
+++ JZoffer/27.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    TreeNode *mirrorTree(TreeNode *root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        root->left = mirrorTree(root->left);
+        root->right = mirrorTree(root->right);
+        auto t = root->left;
+        root->left = root->right;
+        root->right = t;
+        return root;
+    }
+};
